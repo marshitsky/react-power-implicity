cc1cf14c0eeeed8a9d5f815ccafb17906f14bce9
marshitsky <marshitsky@gmail.com>
2022-11-10 20:17:06 +0300

    refactor(project-1): BEM naming, change nav style

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,37 +2,22 @@ import "./slider.scss";
 import { Button } from "../Button/Button";
 
 export const Slider = () => {
-  const filledEllipse =
-    "https://raw.githubusercontent.com/marshitsky/imgsForDiplomaJS/681e522ba7677fdde3140c0ec9a6875560862879/Ellipse_filled.svg";
-  const emptyEllipse =
-    "https://raw.githubusercontent.com/marshitsky/imgsForDiplomaJS/681e522ba7677fdde3140c0ec9a6875560862879/Ellipse_empty.svg";
-
   return (
     <div className="slider">
-      <div className="slider--slide">
-        <p className="slider--slide--main-text">The Power of Simplicity</p>
-        <p className="slider--slide--description">
+      <div className="slider__slide">
+        <h1 className="slider__title">The Power of Simplicity</h1>
+        <p className="slider__subtitle">
           Instead of spending time searching for the right app, our AI will
           bring the right app to you.
         </p>
-        <Button parent="slider--slide-">Learn</Button>
-        <div className="slider--slide--ellipses">
-          <a href="">
-            <img src={emptyEllipse} alt="" />
-          </a>
-          <a href="">
-            <img src={emptyEllipse} alt="" />
-          </a>
-          <a href="">
-            <img src={filledEllipse} alt="" />
-          </a>
-          <a href="">
-            <img src={emptyEllipse} alt="" />
-          </a>
-          <a href="">
-            <img src={emptyEllipse} alt="" />
-          </a>
-        </div>
+        <Button variant="slider">Learn</Button>
+      </div>
+      <div className="slider__dots">
+        <li className="slider__dot"></li>
+        <li className="slider__dot"></li>
+        <li className="slider__dot slider__dot--active"></li>
+        <li className="slider__dot"></li>
+        <li className="slider__dot"></li>
       </div>
     </div>
   );
